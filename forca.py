cc3ac1a7cd2d8f157870a6a20f70e5c5c5f07bd1
Pedro Teixeira <pedro15at@gmail.com>
2023-10-06 10:31:18 -0300

    finalizacao da forca

--- forca.py
+++ forca.py
@@ -1,14 +1,31 @@
+import random
+
 def jogar():
 
     print("*********************************")
     print("Bem vindo no jogo da forca!")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    arquivo =open("palavras.txt","r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+    letras_acertadas = []
+
+    for letra in palavra_secreta:
+        letras_acertadas.append("_")
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas) #mostra ao usuario o tamanho da palavra que esta buscando
 
@@ -16,19 +33,29 @@ def jogar():
     while(not enforcou and not acertou):
 
         chute = input("Digite uma letra: ")
-        chute = chute.strip() # utilizado para retirar espaço que o usuario poderia ter colocado
+        chute = chute.strip().upper() # utilizado para retirar espaço que o usuario poderia ter colocado
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print(f"Encontrei a letra {chute} na posicao {index}")
-                letras_acertadas[index] = letra
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    print(f"Encontrei a letra {chute} na posicao {index}")
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print(f"Ops, voce errou. Faltam {6 - erros} tentativas")
 
-            index = index + 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+
         print(letras_acertadas)
-        print("jogando....")
 
 
+    if(acertou):
+        print('Voce ganhou !')
+    else:
+        print('Voce perdeu \n')
     print("Fim do jogo \n")
 
 if(__name__ == "__main__"):
